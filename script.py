b49ba4031ce8f77c06d3cdc6dcb5a7bcccf89e8f
brucepro <git@brucepro.net>
2024-02-11 13:34:43 -0800

    open ai poc
    working poc with openai

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 """
-Text_gen use api. 
-Accepts a compatible api and ignores generation using the backend. Allows you to just 
-use the frontend and other extensions with text gen and an api server. 
+Text Gen Use API
+Bypasses the generate reply and uses an external api. 
+
 """
 
 import gradio as gr
@@ -23,48 +23,104 @@ from datetime import datetime
 import random
 from requests.auth import HTTPBasicAuth
 import base64
+from openai import OpenAI
+
+from dotenv import load_dotenv
+load_dotenv()
 
 params = {
-    "display_name": "Example Extension",
+    "display_name": "Use External API",
     "is_tab": False,
     "api_reply": "",
 }
 
-
-def gen_from_api(string):
-    AIurl = f"http://127.0.0.1:8080/completion"
-    starttime = time.time()
-    req_json = {
-    "stream": False,
-    "n_predict": 24576,
-    "temperature": 0.7,
-    "stop": [
-        "</s>",
-    ],
-    "repeat_last_n": 256,
-    "repeat_penalty": 1,
-    "top_k": 20,
-    "top_p": 0.75,
-    "tfs_z": 1,
-    "typical_p": 1,
-    "presence_penalty": 0,
-    "frequency_penalty": 0,
-    "mirostat": 0,
-    "mirostat_tau": 5,
-    "mirostat_eta": 0.1,
-    "grammar": "",
-    "n_probs": 0,
-    "prompt": string
+def gen_from_api(string,model,state,type):
+    print(os.environ["OPENAI_API_KEY"])
+    print(state)
+    if type == "OpenAI":
+        response = requests.post("https://api.openai.com/v1/chat/completions",
+            headers={
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + os.environ["OPENAI_API_KEY"]},
+            json={"model": model,
+            "messages": [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": string}]})
+        response_data = json.loads(response.content)
+        params['api_reply'] = response_data["choices"][0]["message"]["content"]
+        return 
+    if type == "Anthropic":
+        response = requests.post(
+    "https://api.anthropic.com/v1/complete",
+    headers={
+        "accept": "application/json",
+        "anthropic-version": "2023-06-01",
+        "content-type": "application/json",
+        "x-api-key": os.environ["ANTHROPIC_API_KEY"]
+    },
+    json={
+        "model": "claude-2.1",
+        "prompt": "\n\nHuman: Hello, world!\n\nAssistant:",
+        "max_tokens_to_sample": 256
     }
+)
+        return str(response.content)
 
-    print(string)
-    res = requests.post(AIurl, json=req_json)
-    result = res.json()["content"]   
-    endtime = time.time()
-    runtime = endtime - starttime
-    print(f"Runtime: {runtime}s")
-    print(result)
-    return result
+    if type == "Cohere":
+        response = requests.post(
+    "https://api.cohere.ai/v1/chat",
+    headers={
+        "accept": "application/json",
+        "content-type": "application/json",
+        "Authorization": f"Bearer {os.environ['COHERE_API_KEY']}"
+    },
+    json={
+        "chat_history": [
+            {"role": "USER", "message": "Who discovered gravity?"},
+            {"role": "CHATBOT", "message": "The man who is widely credited with discovering gravity is Sir Isaac Newton"}
+        ],
+        "message": "What year was he born?",
+        "connectors": [{"id": "web-search"}]
+    }
+)
+        return str(response.content)
+    if type == "Mistral":
+        response = requests.post(
+    "https://api.mistral.ai/v1/chat/completions",
+    headers={
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "Authorization": f"Bearer {os.environ['MISTRAL_API_KEY']}"
+    },
+    json={
+        "model": "mistral-tiny",
+        "messages": [{"role": "user", "content": "Who is the most renowned French writer?"}]
+    }
+)
+        return str(response.content)
+    if type == "Google":
+        response = requests.post(
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=" + os.environ["GOOGLE_API_KEY"],
+    headers={
+        "Content-Type": "application/json"
+    },
+    json={
+        "contents": [
+            {
+                "parts": [
+                    {
+                        "text": "Write a story about a magic backpack."
+                    }
+                ]
+            }
+        ]
+    }
+)
+        return str(response.content)
+    if type == "Custom":
+        return str(response.content)
+    pass
+
 
 
 def input_modifier(string, state, is_chat=False):
@@ -74,7 +130,8 @@ def input_modifier(string, state, is_chat=False):
     In chat mode, it is the same as chat_input_modifier but only applied
     to "text", here called "string", and not to "visible_text".
     """
-    params['api_reply'] = gen_from_api(string)
+    gen_from_api(string,"gpt-3.5-turbo",state,"OpenAI")
+    
     return string
 
 
@@ -85,7 +142,7 @@ def output_modifier(string, state, is_chat=False):
     In chat mode, the modified version goes into history['visible'],
     and the original version goes into history['internal'].
     """
-    print(string)
+    #print(string)
     string = params['api_reply']
     return string
 
